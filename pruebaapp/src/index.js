8f6ebae59cfdc72cc3403e5d5694961ff8ee4dde
zac033zc <abraham_cryter2007@hotmail.com>
2021-12-18 18:57:49 -0600

    Aprendiendo ReacJS

---
+++ pruebaapp/src/index.js
@@ -0,0 +1,31 @@
+// const element = document.createElement('h1')
+// element.innerText = 'Hola mundo con ReactJS'
+// const container = document.getElementById('root')
+// container.appendChild(element)
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const element = <h1>Hola mundo con ReactJS</h1>
+const container = document.getElementById('root')
+
+const name = 'Abraham Cocoletzi Zempoalteca'
+const element2 = <h2>Hola {getName(user)} </h2>
+const container2 = document.getElementById('root2')
+
+const element3 = <div>lorem</div>
+const container3 = document.getElementById('root3')
+
+const user = {
+  firstName: 'Abraham',
+  lastName: 'Cocoletzi'
+}
+
+function getName(user){
+  return `${user.firstName} ${user.lastName}`
+}
+
+// ReactDOM.render(__Que__, __Donde__)
+ReactDOM.render(element, container)
+ReactDOM.render(element2, container2)
+ReactDOM.render(element3, container3)
