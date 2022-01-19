2718dae9f3d4c896af3236c6faa99934da774533
zac033zc <abraham_cryter2007@hotmail.com>
2022-01-19 13:40:20 -0600

    Prueba

--- pruebaapp/src/index.js
+++ pruebaapp/src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 // const element = document.createElement('h1')
 // element.innerText = 'Hola mundo con ReactJS'
 // const container = document.getElementById('root')
@@ -9,7 +10,7 @@ import ReactDOM from "react-dom";
 const element = <h1>Hola mundo con ReactJS</h1>
 const container = document.getElementById('root')
 
-const name = 'Abraham Cocoletzi Zempoalteca'
+// const name = 'Abraham Cocoletzi Zempoalteca'
 const element2 = <h2>Hola {getName(user)} </h2>
 const container2 = document.getElementById('root2')
 
